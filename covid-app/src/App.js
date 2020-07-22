460670d7cfc6c523ea78e02cfaf60f47ec954e5c
Noah Baltensperger <nbalten@gmail.com>
2020-07-22 00:44:36 -0500

    Update App.js
    Wednesday 4 hours

--- covid-app/src/App.js
+++ covid-app/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Chartdata from './components/Chartdata';
+import Navbar from './components/Navbar';
+import Listdata from './components/Listdata';
+import './App.css';
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Navbar />
          <Chartdata />
+         <Listdata />
       </div>
     )
   }
